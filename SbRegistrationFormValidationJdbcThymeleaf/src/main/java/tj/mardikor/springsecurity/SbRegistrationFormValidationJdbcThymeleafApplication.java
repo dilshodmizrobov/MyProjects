39d0cceaadc7b8497cfbb3c46b8cc58c79ea6967
dilshodmizrobov1994 <dilshod.mizrobov@inbox.ru>
2019-09-02 17:07:22 +0500

    Validation Form

---
+++ SbRegistrationFormValidationJdbcThymeleaf/src/main/java/tj/mardikor/springsecurity/SbRegistrationFormValidationJdbcThymeleafApplication.java
@@ -0,0 +1,13 @@
+package tj.mardikor.springsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbRegistrationFormValidationJdbcThymeleafApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbRegistrationFormValidationJdbcThymeleafApplication.class, args);
+	}
+
+}
---
+++ SbRegistrationFormValidationJdbcThymeleaf/src/test/java/tj/mardikor/springsecurity/SbRegistrationFormValidationJdbcThymeleafApplicationTests.java
@@ -0,0 +1,16 @@
+package tj.mardikor.springsecurity;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SbRegistrationFormValidationJdbcThymeleafApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
